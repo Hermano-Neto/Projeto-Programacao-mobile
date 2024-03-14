0c833d3638f9ba73252fcb96d7ee43a024571464
Hermano Neto <hermano.neto@academico.ifpb.edu.br>
2024-03-14 18:14:39 -0300

    ContruÃção da tela inicial

--- app/src/main/java/com/example/projeto/MainActivity.kt
+++ app/src/main/java/com/example/projeto/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.projeto
 
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)a
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+        window.statusBarColor = Color.parseColor("#FF000000")
+
     }
 }
