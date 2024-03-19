1270d7e25041c6127d27fc742abd993475bdab7f
Hermano Neto <hermano.neto@academico.ifpb.edu.br>
2024-03-19 20:45:21 -0300

    add tela de aplicativos

--- app/src/main/java/com/example/projeto/TelaLogin.kt
+++ app/src/main/java/com/example/projeto/TelaLogin.kt
@@ -1,5 +1,6 @@
 package com.example.projeto
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,8 @@ class TelaLogin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela_login)
+
+        supportActionBar?.hide()
+        window.statusBarColor = Color.parseColor("#FF000000")
     }
 }
