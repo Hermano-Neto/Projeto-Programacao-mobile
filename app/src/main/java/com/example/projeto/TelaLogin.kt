c8755ecef52c95dc3394f41686fb19f3e15d9a5a
Hermano Neto <hermano.neto@academico.ifpb.edu.br>
2024-03-20 20:20:48 -0300

    Botao de login funcional, Menum sem funcionar

--- app/src/main/java/com/example/projeto/TelaLogin.kt
+++ app/src/main/java/com/example/projeto/TelaLogin.kt
@@ -1,8 +1,10 @@
 package com.example.projeto
 
+import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class TelaLogin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +13,13 @@ class TelaLogin : AppCompatActivity() {
 
         supportActionBar?.hide()
         window.statusBarColor = Color.parseColor("#FF000000")
+
+        val loginButton = findViewById<Button>(R.id.loginButton)
+        loginButton.setOnClickListener {
+            // Aqui você pode iniciar uma nova atividade ou executar qualquer ação desejada
+            // Por exemplo, iniciar uma nova atividade após clicar no botão de login
+            val intent = Intent(this, Menu::class.java)
+            startActivity(intent)
+        }
     }
 }
